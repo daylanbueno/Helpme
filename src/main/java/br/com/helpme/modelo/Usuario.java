2a268ea080c17574864992224dabd02498e03cf4
Dailan Bueno <daylansantos@gmail.com>
2017-06-19 22:06:42 -0300

    hibernate.cfg

---
+++ src/main/java/br/com/helpme/modelo/Usuario.java
@@ -0,0 +1,45 @@
+package br.com.helpme.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name="TB_USUARIO")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column(nullable = false,length=70)
+	private String email;
+	
+	@Column(nullable=false, length=40)
+	private String senha;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	} 
+
+}
