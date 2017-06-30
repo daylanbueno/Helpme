27dfbe9cac88818528bdeeae34078f29c271e015
Dailan Bueno <daylansantos@gmail.com>
2017-06-29 22:50:12 -0300

    alterações script e consulta municipio estado.

--- src/main/java/br/com/helpme/controller/ClienteBean.java
+++ src/main/java/br/com/helpme/controller/ClienteBean.java
@@ -8,21 +8,30 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.helpme.dao.EstadoDao;
+import br.com.helpme.dao.MunicipioDao;
 import br.com.helpme.modelo.Estado;
+import br.com.helpme.modelo.Municipio;
 
 @Named
 @RequestScoped
 public class ClienteBean {
 	
+	private Estado estadoSelecionado;
+	
 	private List<Estado> listaEstados;
+	
+	private List<Municipio> listaMunicipios;
 	
 	@Inject
 	private EstadoDao estadoDao;
 
+	@Inject
+	private MunicipioDao municipioDao;
+	
 	@PostConstruct
 	public void carregarEstados(){
 		setListaEstados(estadoDao.carregarEstados());
-		System.out.println("carregou os estados");
+		System.out.println("carregou os estados=>"+getEstadoSelecionado().getNome());
 	}
 	
 	public List<Estado> getListaEstados() {
@@ -32,8 +41,32 @@ public class ClienteBean {
 	public void setListaEstados(List<Estado> listaEstados) {
 		this.listaEstados = listaEstados;
 	}
+
+	public Estado getEstadoSelecionado() {
+		if(estadoSelecionado== null){
+			estadoSelecionado = new Estado();
+		}
+		return estadoSelecionado;
+	}
+
+	public void setEstadoSelecionado(Estado estadoSelecionado) {
+		this.estadoSelecionado = estadoSelecionado;
+	}
 	
 	
+	public List<Municipio> getListaMunicipios() {
+		return listaMunicipios;
+	}
+
+	public void setListaMunicipios(List<Municipio> listaMunicipios) {
+		this.listaMunicipios = listaMunicipios;
+	}
+
+	public void carregarMunicipioPorEstado(){
+		if(getEstadoSelecionado()!=null){
+			setListaMunicipios(municipioDao.findMunicipiosByEstado(getEstadoSelecionado()));
+		}
+	}
 	
 
 }
--- src/main/java/br/com/helpme/dao/MunicipioDao.java
+++ src/main/java/br/com/helpme/dao/MunicipioDao.java
@@ -1,14 +1,37 @@
 package br.com.helpme.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import javax.faces.event.ExceptionQueuedEvent;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.hibernate.QueryException;
+
+import br.com.helpme.modelo.Estado;
+import br.com.helpme.modelo.Municipio;
 
 public class MunicipioDao {
 
 	@Inject
 	private EntityManager manager;
-		
+
+	
+	@SuppressWarnings("unchecked")
+	public  List<Municipio> findMunicipiosByEstado(Estado estado){
+		List<Municipio> lista =null;
+		try {
+			String jpql = "select m from Municipio m  where m.estado =:pEstado";
+			Query query = manager.createQuery(jpql);
+			query.setParameter("pEstado", estado);
+			lista = query.getResultList();
+			
+		} catch (QueryException e) {
+			e.printStackTrace();
+		}
+		return lista;
+	}
 
 }
