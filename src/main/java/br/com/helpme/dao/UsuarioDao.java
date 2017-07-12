1604b95b0844d962150e7bf3e9578112e4d8db0c
Dailan Bueno <daylansantos@gmail.com>
2017-07-11 21:13:04 -0300

    AUTETICAÇÃOD E USUÁRIO NO SISTEMA

--- src/main/java/br/com/helpme/dao/UsuarioDao.java
+++ src/main/java/br/com/helpme/dao/UsuarioDao.java
@@ -1,5 +1,7 @@
 package br.com.helpme.dao;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -14,15 +16,20 @@ public class UsuarioDao {
 	
 	public Usuario findUsuarioByUsuario(Usuario user){
 		StringBuilder jpql = new  StringBuilder();
-		jpql.append("SELECT usuario FROM Usuario usuario where usuario.email =: paramEmail");
+		jpql.append("SELECT usuario FROM Usuario usuario where usuario.email =:paramEmail");
 		jpql.append(" AND usuario.senha =:paramSenha");
 		
 		Query query  = manager.createQuery(jpql.toString());
 		query.setParameter("paramEmail", user.getEmail());
 		query.setParameter("paramSenha", user.getSenha());
 	
-		return (Usuario) query.getResultList();
-	
+		List<Usuario> usuarios =  query.getResultList();
+		if(usuarios !=null &&  !usuarios.isEmpty()){
+			return usuarios.get(0);
+		}else{
+			return null;
+		}
+		
 	}
 
 }
